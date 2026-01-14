a364d311ff4b8dde9c3130e07eae6e2cbc26d184
sra-unlu <serraunlu.e@gmail.com>
2026-01-13 23:06:54 -0500

    idk

--- anagrams/AnagramFinder.java
+++ anagrams/AnagramFinder.java
@@ -13,6 +13,16 @@ public class AnagramFinder {
     return anagrams;
   }
 
+  private static void getAllAnagrams(String prefix, String word, List<String> anagramList) {
+    if (word.length() == 0)
+      anagramList.add(prefix);
+    for (int i = 0; i < word.length(); i++) {
+      char current = word.charAt(i);
+      String wordRemaining = word.substring(0, i) + word.substring(i + 1);
+      getAllAnagrams(prefix + current, wordRemaining, anagramList);
+    }
+  }
+
   private static void permutation(String prefix, String remaining, LookupList dict, List<String> anagrams) {
     if (remaining.length() == 0) {
       if (dict.contains(prefix) && !anagrams.contains(prefix)) {
